963b4d602eeaee258a7e8a8e4ce474d730b86b2c
Aaron Peck <aaronpeck_14@yahoo.com>
2023-03-31 17:22:40 -0500

    change color of last seen mobile

--- components/Tables/ClanMembers.js
+++ components/Tables/ClanMembers.js
@@ -249,7 +249,7 @@ export default function MembersTable({ members }) {
 												color: gray["25"]
 											}}>{formatRole(m.role)}</span>
 											<span style={{
-												color: gray["25"]
+												color: gray["50"]
 											}}>{m.lastSeenStr}</span>
 										</BottomMobileDiv>
 									</MobileNameCell> :
