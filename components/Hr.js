b054ff1ca50066f6579a24ed3ccd7f6fa43e2ff5
Aaron Peck <aaronpeck_14@yahoo.com>
2023-04-11 23:27:29 -0500

    increased default height

--- components/Hr.js
+++ components/Hr.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 import { gray } from "../public/static/colors"
 
 const Main = styled.div`
-  height: ${({ height }) => height || "1px"};
+  height: ${({ height }) => height || "2px"};
   width: ${({ width }) => width || "100%"};
   background-color: ${({ color }) => color || gray["75"]};
 `
