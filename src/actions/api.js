46b584167abe522596551529a4f85d34ce8ca99c
Aaron Peck <aaronpeck_14@yahoo.com>
2024-01-30 23:38:42 -0500

    fix: testing headers

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,12 +1,12 @@
 "use server"
 
 /* eslint-disable import/prefer-default-export */
-import { headers } from "next/headers"
+// import { headers } from "next/headers"
 
 const HOST = process.env.NEXTAUTH_URL
 
 export async function getLinkedAccount() {
-  const options = { cache: "no-store", headers: headers() }
+  const options = { cache: "no-store" }
   const linkedAccount = await fetch(`${HOST}/api/user`, options)
 
   return linkedAccount.json()
