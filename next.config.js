beb1cf4dca0c69c76d49a5ff23b208bdfe76d398
Aaron Peck <aaronpeck_14@yahoo.com>
2024-03-12 21:34:05 -0400

    fix: updated packages, removed optimizePackageImports

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withAxiom } = require("next-axiom")
 const headers = require("./headers")
 
 module.exports = withAxiom({
-  experimental: {
-    optimizePackageImports: ["@mantine/core", "@mantine/dates", "@mantine/form", "@mantine/hooks"],
-  },
   headers: async () => [
     {
       headers,
