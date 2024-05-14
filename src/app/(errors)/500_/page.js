6a468b516d9a40da7d971375a2ec63ff6787248f
Aaron Peck <aaronpeck_14@yahoo.com>
2024-05-13 23:32:21 -0400

    feat: added invite links to error pages

--- src/app/(errors)/500_/page.js
+++ src/app/(errors)/500_/page.js
@@ -1,10 +1,20 @@
+import Link from "next/link"
+
 import ErrorPage from "@/components/error-page"
 
 export default function Page500() {
   return (
     <ErrorPage
       code={500}
-      description="Something went wrong behind the scenes. If this issue persists, please join the Support Server."
+      description={
+        <>
+          Something went wrong behind the scenes. If this issue persists, please join the{" "}
+          <Link className="pinkText" href="/support">
+            Support Server
+          </Link>
+          .
+        </>
+      }
       title="Unexpected error."
     />
   )
