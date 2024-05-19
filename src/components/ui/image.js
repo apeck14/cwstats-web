e899e10c6b5c3b2690d8eff013359493ed85c10b
Aaron Peck <aaronpeck_14@yahoo.com>
2024-05-18 23:26:05 -0400

    fix: fix images on firefox

--- src/components/ui/image.js
+++ src/components/ui/image.js
@@ -14,26 +14,29 @@ export default function Image({
   priority,
   radius,
   src,
+  style,
   unoptimized,
   width,
 }) {
   return (
-    <MantineImage
-      alt={alt}
-      className={className || ""}
-      component={NextImage}
-      fill={fill}
-      fit={fit || "contain"}
-      height={height}
-      hiddenFrom={hiddenFrom}
-      onError={onError}
-      onLoad={onLoad}
-      priority={priority}
-      radius={radius}
-      src={src}
-      style={circle ? { borderRadius: "50%" } : {}}
-      unoptimized={unoptimized}
-      width={circle ? height : width || height}
-    />
+    <div style={{ position: "relative" }}>
+      <MantineImage
+        alt={alt}
+        className={className || ""}
+        component={NextImage}
+        fill={fill}
+        fit={fit || "contain"}
+        height={height}
+        hiddenFrom={hiddenFrom}
+        onError={onError}
+        onLoad={onLoad}
+        priority={priority}
+        radius={radius}
+        src={src}
+        style={circle ? { ...style, borderRadius: "50%" } : { ...style }}
+        unoptimized={unoptimized}
+        width={circle ? height : width || height}
+      />
+    </div>
   )
 }
