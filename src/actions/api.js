82a423b94dfe1693c82483a9575f4855a58867e1
Aaron Peck <aaronpeck_14@yahoo.com>
2024-07-16 02:15:20 -0400

    fix: chunk plus clan reqs

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,5 +1,6 @@
 "use server"
 
+import { chunk } from "lodash"
 import { Logger } from "next-axiom"
 
 import { getDaysDiff, parseDate } from "@/lib/functions/date-time"
@@ -34,7 +35,6 @@ export async function getAllDailyLbClans() {
     const client = await clientPromise
     const db = client.db("General")
     const CWStatsPlus = db.collection("CWStats+")
-    const cwstatsPlusTags = await getAllPlusClans(true)
 
     const trackedLocationIDs = locations.filter((l) => l.isAdded || l.name === "Global").map((l) => l.id)
 
@@ -57,37 +57,46 @@ export async function getAllDailyLbClans() {
       }))
       .filter((c) => c.clanScore >= 4000)
 
-    for (const tag of cwstatsPlusTags) {
-      const { data: clan, error } = await getClan(tag)
-      if (error || !clan) continue
+    const cwstatsPlusTags = await getAllPlusClans(true)
+    const chunkedPlusTags = chunk(cwstatsPlusTags, 3)
 
-      // remove clan from plus
-      if (clan.members === 0 || !clan.description.toLowerCase().includes("cwstats") || clan.clanWarTrophies < 2800) {
-        CWStatsPlus.deleteOne({ tag })
-        sendPlusWebhookEmbed("REMOVE_PLUS", {
-          name: clan.name,
-          tag: clan.tag,
+    for (const trio of chunkedPlusTags) {
+      const clanPromises = trio.map((tag) => getClan(tag))
+      const clans = await Promise.all(clanPromises)
+
+      for (const { data: clan, error } of clans) {
+        if (error || !clan) continue
+
+        const { badgeId, clanWarTrophies, description, location, members, name, tag } = clan
+
+        // remove clan from plus
+        if (members === 0 || !description.toLowerCase().includes("cwstats") || clanWarTrophies < 2800) {
+          CWStatsPlus.deleteOne({ tag })
+          sendPlusWebhookEmbed("REMOVE_PLUS", {
+            name,
+            tag,
+          })
+          continue
+        }
+
+        // add plus to lb clans that are plus clans
+        const clanFound = allLbClans.find((c) => c.tag === tag)
+
+        if (clanFound) {
+          clanFound.plus = true
+          continue
+        }
+
+        // add new entry for plus clans that are not on war lbs
+        allLbClans.push({
+          badgeId,
+          clanScore: clanWarTrophies,
+          location,
+          name,
+          plus: true,
+          tag,
         })
-        continue
       }
-
-      // add plus to lb clans that are plus clans
-      const clanFound = allLbClans.find((c) => c.tag === tag)
-
-      if (clanFound) {
-        clanFound.plus = true
-        continue
-      }
-
-      // add new entry for plus clans that are not on war lbs
-      allLbClans.push({
-        badgeId: clan.badgeId,
-        clanScore: clan.clanWarTrophies,
-        location: clan.location,
-        name: clan.name,
-        plus: true,
-        tag,
-      })
     }
 
     return allLbClans
