7365c1c7c64f01e5f2ea538180ca04428326a95b
Aaron Peck <aaronpeck_14@yahoo.com>
2024-10-19 21:54:50 -0700

    fix: show errors

--- src/components/me/players/add-by-clan-modal.js
+++ src/components/me/players/add-by-clan-modal.js
@@ -64,7 +64,7 @@ export default function AddByClanModal({ guildID, linkedAccounts, linksRemaining
       if (player?.added) {
         playersAdded.push(player)
       } else {
-        newUnlinkedMembers.push(m)
+        newUnlinkedMembers.push({ ...m, ...player })
       }
     }
 
