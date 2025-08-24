0898258d2fbce6383a63acd81c3e6461b8efded2
Aaron Peck <aaronpeck_14@yahoo.com>
2025-08-23 21:48:56 -0700

    feat: update feature cards

--- src/components/upgrade/feature-cards.js
+++ src/components/upgrade/feature-cards.js
@@ -1,13 +1,22 @@
 "use client"
 
-import { Button, Divider, Group, Paper, Popover, SimpleGrid, Stack, Text, Title } from "@mantine/core"
+import { Button, Card, Divider, Flex, Group, Paper, Popover, SimpleGrid, Stack, Text, Title } from "@mantine/core"
 import { useMediaQuery } from "@mantine/hooks"
-import { IconCheck } from "@tabler/icons-react"
+import { IconCircleCheck } from "@tabler/icons-react"
 
 import PlusFormModal from "./plus-form-modal"
+import classes from "./upgrade.module.css"
 
 const options = [
   {
+    button: (
+      <Card align="center" className={classes["striped-gray"]} component={Flex} h="2.25rem" justify="center" p={0}>
+        <Text c="gray.5" fw={600}>
+          Base Plan
+        </Text>
+      </Card>
+    ),
+    color: "var(--mantine-color-gray-3)",
     featureHeader: "Get started with:",
     features: [
       { text: "Full access to Discord Bot" },
@@ -18,22 +27,24 @@ const options = [
       { subText: "15 Abbreviations", text: "Clan abbreviations" },
       { subText: "3 Scheduled Nudges • 100 Linked Players", text: "Nudges via Discord" },
     ],
-    subtitle: "Perfect for casual war clans.",
-    title: "STANDARD",
+    price: "FREE",
+    subtitle: "Perfect for casual war clans looking to enhance their war experience.",
+    title: "Standard",
   },
   {
     button: <PlusFormModal />,
-    featureHeader: "All Standard features, plus:",
+    color: "var(--mantine-color-orange-5)",
+    featureHeader: "All STANDARD features, plus:",
     features: [
+      { subText: "Daily & weekly player performance tracking.", text: "Player tracking" },
       { text: "Hourly clan average tracking" },
-      { text: "Daily player score tracking" },
-      { text: "Verified checkmark" },
-      { text: "Daily Leaderboard clan accolades" },
+      { text: "Plus checkmark" },
       { text: "Guaranteed spot on Daily Leaderboard" },
       { subText: "+2 Scheduled Nudges • +75 Linked Players", text: "Increased Nudge Limits" },
     ],
+    price: "FREE",
     subtitle: "For competitive war clans looking to dive into rich CW2 features.",
-    title: "PLUS",
+    title: "Plus",
   },
   {
     button: (
@@ -50,15 +61,24 @@ const options = [
         </Popover.Target>
         <Popover.Dropdown>
           <Text size="sm" ta="center">
-            Pro coming soon! 🎉
+            Upgrade from your server dashboard! 🎉
           </Text>
         </Popover.Dropdown>
       </Popover>
     ),
-    featureHeader: "All Plus features, plus:",
-    features: [{ text: "Deep analytics coming soon 🎉" }],
-    subtitle: "Coming soon 🎉",
-    title: "PRO",
+    color: "var(--mantine-color-pink-5)",
+    featureHeader: "All PLUS features, plus:",
+    features: [
+      { subText: "Real-time battle logs posted in your Discord server", text: "War Logs" },
+      { text: "Global clan abbreviation" },
+      { text: "Special role in Support Server" },
+      { text: "Pro checkmark" },
+      { text: "No clan description or trophy requirement" },
+      { text: "& more coming soon! 🎉" },
+    ],
+    price: "$2.50",
+    subtitle: "Unlock the full power of CWStats with advanced features.",
+    title: "Pro",
   },
 ]
 
@@ -71,10 +91,21 @@ export default function FeatureCards() {
         <Paper bd="1px solid gray.6" bg="transparent" key={c.title} maw="25rem" mih="30rem" p="xl" radius="md">
           <Stack justify="space-between" mih={isLaptop ? 0 : "10rem"}>
             <Stack gap={0}>
-              <Title fw={600} mb="sm" size="h2">
+              <Title c={c.color} fw={600} size="h2">
                 {c.title}
               </Title>
-              <Text c="gray.1" fw={600} fz="0.95rem" lh="xs" mb="lg">
+              <Group align="end" gap="0.25rem" my="xs">
+                <Text c="white" fw={800} size="1.75rem">
+                  {c.price}
+                </Text>
+                {c.price !== "FREE" && (
+                  <Text c="gray.2" fw={600}>
+                    /month
+                  </Text>
+                )}
+              </Group>
+
+              <Text c="gray.4" fz="0.9rem" mb="lg">
                 {c.subtitle}
               </Text>
             </Stack>
@@ -90,7 +121,7 @@ export default function FeatureCards() {
           <Stack gap="xs">
             {c.features.map((f) => (
               <Group align="flex-start" gap="xs" key={f.text} wrap="nowrap">
-                <IconCheck color="var(--mantine-color-green-5)" size="1.2rem" />
+                <IconCircleCheck color={c.color} size="1.2rem" />
 
                 <Stack gap="0">
                   <Text fw={500} fz="0.9rem">
