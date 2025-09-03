42856885ade96a5775f354befe5235f6410ef832
Aaron Peck <aaronpeck_14@yahoo.com>
2025-09-03 04:04:56 -0700

    fix: dynamically load success page

--- src/app/checkout/success/page.js
+++ src/app/checkout/success/page.js
@@ -10,6 +10,8 @@ export const metadata = {
   title: "Success | CWStats Pro",
 }
 
+export const dynamic = "force-dynamic"
+
 export default async function CheckoutSuccessPage({ searchParams }) {
   const { sessionId } = searchParams
 
