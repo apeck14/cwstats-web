f1a8b5a138dee3e6c736998badd1b2b2618ddbc2
Aaron Peck <aaronpeck_14@yahoo.com>
2025-09-03 20:26:36 -0700

    fix: get guildId from subscription metadata

--- src/actions/stripe.js
+++ src/actions/stripe.js
@@ -9,8 +9,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 export const getStripeSession = async (sessionId) => {
   try {
     const session = await stripe.checkout.sessions.retrieve(sessionId)
+    const subscription = await stripe.subscriptions.retrieve(session.subscription)
 
-    const guildId = session.metadata?.guildId
+    const guildId = subscription?.metadata?.guildId
 
     if (guildId) {
       return { url: `/me/servers/${guildId}/clans` }
